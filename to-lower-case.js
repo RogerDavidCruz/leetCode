320be5016f690ae92377afc061261dcc515996e4
Roger David Cruz <roger.david.cruz@gmail.com>
2019-08-09 23:31:18 -0400

    first leetcode

---
+++ to-lower-case.js
@@ -0,0 +1,16 @@
+// 709. To Lower Case
+//
+// Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+//
+// Example 1:
+//
+// Input: "Hello"
+// Output: "hello"
+//
+/**
+ * @param {string} str
+ * @return {string}
+ */
+var toLowerCase = function(str) {
+    return str.toLowerCase()
+};
